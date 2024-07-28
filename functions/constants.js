9cd9129080ca818ec073160b386fcae2696126ef
abdullah-aa <abdullaha90@gmail.com>
2024-07-28 15:23:50 -0400

    Split getImage code into multiple functions.

--- functions/constants.js
+++ functions/constants.js
@@ -1,4 +1,7 @@
 const INTERESTS = ["Business", "Entertainment", "Politics", "Sports"];
 const TO_DELETE_KEY = "to_delete";
 
-module.exports = { INTERESTS, TO_DELETE_KEY };
+// https://firebase.google.com/docs/firestore/query-data/queries#limits_on_or_queries
+const MAX_LEFT_HAND_LIST_SIZE = 30;
+
+module.exports = { INTERESTS, TO_DELETE_KEY, MAX_LEFT_HAND_LIST_SIZE };

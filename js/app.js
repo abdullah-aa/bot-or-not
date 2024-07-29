db2dcd2a9e6d4c390edf151ebe67175045778193
abdullah-aa <abdullaha90@gmail.com>
2024-07-28 20:07:05 -0400

    Image retrieval and rendering on UI complete.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
+import { getFunctions, httpsCallable } from "firebase/functions";
 import {
-  getAuth,
   createUserWithEmailAndPassword,
+  getAuth,
   signInWithEmailAndPassword,
-} from 'firebase/auth';
+} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCGzjPbE3MCph6S-3xwPS7Pp_X7a72btr4",
@@ -11,7 +12,7 @@ const firebaseConfig = {
   projectId: "bot-or-not-erg237",
   storageBucket: "bot-or-not-erg237.appspot.com",
   messagingSenderId: "195354824452",
-  appId: "1:195354824452:web:f087178204f04d785a8a51"
+  appId: "1:195354824452:web:f087178204f04d785a8a51",
 };
 
 const STATE_MAP = {
@@ -24,6 +25,7 @@ const STATE_MAP = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const functions = getFunctions(app);
 
 const email = document.getElementById('email');
 const password = document.getElementById('password');
@@ -36,7 +38,12 @@ const loginButton = document.getElementById('loginButton');
 const authButtons = document.getElementById('authButtons');
 
 const interestsForm = document.getElementById('interestsForm');
-const interestHeader = document.getElementById('interestHeader');
+
+const loadingScreen = document.getElementById('loadingScreen');
+
+const promptScreen = document.getElementById('promptScreen');
+const promptImage = document.getElementById('promptImage');
+const promptText = document.getElementById('promptText');
 
 const errorDiv = document.getElementById('errorDiv');
 const errorCode = document.getElementById('errorCode');
@@ -44,10 +51,16 @@ const errorMessage = document.getElementById('errorMessage');
 
 let _USER, _AUTH_MODE;
 
-const renderError = (error) => {
-  errorDiv.classList.remove('hidden');
-  errorCode.innerText = error.code;
-  errorMessage.innerText = error.message;
+const renderError = (error = null) => {
+    if (error) {
+        errorDiv.classList.remove('hidden');
+        errorCode.innerText = error.code;
+        errorMessage.innerText = error.message;
+    } else {
+        errorDiv.classList.add('hidden');
+        errorCode.innerText = '';
+        errorMessage.innerText = '';
+    }
 };
 
 signupButton.addEventListener('click', () => {
@@ -66,31 +79,51 @@ loginButton.addEventListener('click', () => {
   _AUTH_MODE = STATE_MAP.LOGIN;
 });
 
-authForm.addEventListener('submit', async (event) => {
+authForm.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  const revealPostLogin = (newMode) => {
+  const showInterestSelection = (userCredential) => {
+    _USER = userCredential.user;
+    _AUTH_MODE = STATE_MAP.LOGGED_IN;
+
     authButtons.classList.add('hidden');
     authForm.classList.add('hidden');
-
-    _AUTH_MODE = newMode;
+    renderError();
 
     interestsForm.classList.remove('hidden');
   };
 
   if (_AUTH_MODE === 'signup') {
     createUserWithEmailAndPassword(auth, email.value, password.value)
-      .then((userCredential) => {
-        _USER = userCredential.user;
-        revealPostLogin(STATE_MAP.NEW_LOGIN);
-      })
+      .then(showInterestSelection)
       .catch(renderError);
   } else {
     signInWithEmailAndPassword(auth, email.value, password.value)
-      .then((userCredential) => {
-        _USER = userCredential.user;
-        revealPostLogin(STATE_MAP.LOGGED_IN);
-      })
+      .then(showInterestSelection)
       .catch(renderError);
   }
+});
+
+interestsForm.addEventListener('submit',(event) => {
+    event.preventDefault();
+
+    interestsForm.classList.add('hidden');
+
+    const interestData = new FormData(event.target);
+    const interest = interestData.get('interest');
+
+    loadingScreen.classList.remove('hidden');
+
+    const getImage = httpsCallable(functions, 'getImage');
+    getImage({ interest })
+      .then((result) => {
+          loadingScreen.classList.add('hidden');
+
+          promptImage.src = result.data.imageUrl;
+          promptText.innerText = result.data.description && result.data.description.replace('[...]', '');
+          promptScreen.classList.remove('hidden');
+
+          alert(result.data.id);
+      })
+      .catch(renderError);
 });

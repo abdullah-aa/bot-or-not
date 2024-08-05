9d220e5769b6f00c4aed0381e5f1a371acf6addb
abdullah-aa <abdullaha90@gmail.com>
2024-08-05 16:02:20 -0400

    Changes app scrip to use async/awaits - bugfixes.

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ import {
   signInWithEmailAndPassword,
 } from "firebase/auth";
 
+// Constants, Variables Firebase Init -- Begin
 const firebaseConfig = {
   apiKey: "AIzaSyCGzjPbE3MCph6S-3xwPS7Pp_X7a72btr4",
   authDomain: "bot-or-not-erg237.firebaseapp.com",
@@ -15,12 +16,16 @@ const firebaseConfig = {
   appId: "1:195354824452:web:f087178204f04d785a8a51",
 };
 
-const MAX_PROMPT_LENGTH = 150;
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const functions = getFunctions(app);
 
+const MAX_PROMPT_LENGTH = 150;
+
+let _USER, _INTEREST, _RESPONSE;
+// End -- Constants, Variables Firebase Init
+
+// Page Elements -- Begin
 const welcomeForm = document.getElementById("welcomeForm");
 const authForm = document.getElementById("authForm");
 
@@ -45,11 +50,9 @@ const errorCode = document.getElementById("errorCode");
 const errorMessage = document.getElementById("errorMessage");
 
 const resetButton = document.getElementById("resetButton");
+// End -- Page Elements
 
-let _USER, _INTEREST, _RESPONSE;
-
-// Helpers
-
+// Helpers/Utilities -- Begin
 const renderError = (error = null) => {
   if (error) {
     errorDiv.classList.remove("hidden");
@@ -69,53 +72,61 @@ const updateCharCount = () => {
   } left`;
 };
 
-const getPromptImage = () => {
+const getPromptImage = async () => {
   promptForm.classList.add("hidden");
   loadingScreen.classList.remove("hidden");
 
-  const getImage = httpsCallable(functions, "getImage");
-  getImage({ interest: _INTEREST })
-    .then((response) => {
-      _RESPONSE = response.data.result;
+  try {
+    const getImage = httpsCallable(functions, "getImage");
+    const response = await getImage({ interest: _INTEREST });
+    _RESPONSE = response.data.result;
 
-      promptImage.src = _RESPONSE.imageUrl;
-      promptText.innerHTML = _RESPONSE.description
-        ? `<h5><q>${_RESPONSE.description.replace("[...]", "")}</q></h5>`
-        : "<h6>No description available, sorry... 😓</h6>";
-      promptInput.value = "";
-      updateCharCount();
+    promptImage.src = _RESPONSE.imageUrl;
+    promptText.innerHTML = _RESPONSE.description
+      ? `<h5><q>${_RESPONSE.description.replace("[...]", "")}</q></h5>`
+      : "<h6>No description available, sorry... 😓</h6>";
+    promptInput.value = "";
+    updateCharCount();
+  } catch (error) {
+    renderError(error);
+  }
 
-      loadingScreen.classList.add("hidden");
-      promptForm.classList.remove("hidden");
-    })
-    .catch(renderError);
+  loadingScreen.classList.add("hidden");
+  promptForm.classList.remove("hidden");
 };
 
-const getChallenge = () => {
+const getChallenge = async (lastGuessResponse) => {
+  if (lastGuessResponse && lastGuessResponse.data) {
+    if (lastGuessResponse.data.result) {
+      alert("You guessed correctly! 🎉");
+    } else {
+      alert("You guessed incorrectly! 😢");
+    }
+  }
+
   guessForm.classList.add("hidden");
   loadingScreen.classList.remove("hidden");
 
-  const getChallenge = httpsCallable(functions, "getChallenge");
-  getChallenge({ interest: _INTEREST })
-    .then((response) => {
-      _RESPONSE = response.data.result;
+  try {
+    const getChallenge = httpsCallable(functions, "getChallenge");
+    const response = await getChallenge({ interest: _INTEREST });
+    _RESPONSE = response.data.result;
 
-      guessImage.src = _RESPONSE.imageUrl;
-      guessPrompt.innerHTML = _RESPONSE.prompt;
-      guessText.innerHTML = _RESPONSE.description
-        ? `written after reading this <h5><q>${_RESPONSE.description.replace(
-            "[...]",
-            ""
-          )}</q></h5>`
-        : "<h6>No context was provided for this image... 😓</h6>";
+    guessImage.src = _RESPONSE.imageUrl;
+    guessPrompt.innerHTML = _RESPONSE.prompt;
+    guessText.innerHTML = _RESPONSE.description
+      ? `written after reading this <h5><q>${_RESPONSE.description.replace(
+          "[...]",
+          ""
+        )}</q></h5>`
+      : "<h6>No context was provided for this image... 😓</h6>";
+  } catch (error) {
+    renderError(error);
+  }
 
-      loadingScreen.classList.add("hidden");
-      guessForm.classList.remove("hidden");
-    })
-    .catch(renderError);
+  loadingScreen.classList.add("hidden");
+  guessForm.classList.remove("hidden");
 };
-
-// Form Listeners
 
 resetButton.addEventListener("click", () => {
   const reset = httpsCallable(functions, "reset");
@@ -126,49 +137,55 @@ resetButton.addEventListener("click", () => {
     })
     .catch(renderError);
 });
+// End -- Helpers/Utilities
 
+// Welcome Form Listeners -- Begin
 welcomeForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
   welcomeForm.classList.add("hidden");
   authForm.classList.remove("hidden");
 });
+// End -- Welcome Form Listeners
 
-authForm.addEventListener("submit", (event) => {
+// Auth Form Listeners -- Begin
+authForm.addEventListener("submit", async (event) => {
   event.preventDefault();
 
   const authData = new FormData(event.target);
-  let authPromise;
+  let userCredential;
 
-  switch (event.submitter.name) {
-    case "signupButton":
-      authPromise = createUserWithEmailAndPassword(
-        auth,
-        authData.get("email"),
-        authData.get("password")
-      );
-      break;
-    case "loginButton":
-      authPromise = signInWithEmailAndPassword(
-        auth,
-        authData.get("email"),
-        authData.get("password")
-      );
-      break;
+  try {
+    switch (event.submitter.name) {
+      case "signupButton":
+        userCredential = await createUserWithEmailAndPassword(
+          auth,
+          authData.get("email"),
+          authData.get("password")
+        );
+        break;
+      case "loginButton":
+        userCredential = await signInWithEmailAndPassword(
+          auth,
+          authData.get("email"),
+          authData.get("password")
+        );
+        break;
+    }
+
+    _USER = userCredential.user;
+
+    renderError();
+
+    authForm.classList.add("hidden");
+    interestsForm.classList.remove("hidden");
+  } catch (error) {
+    renderError(error);
   }
-
-  authPromise
-    .then((userCredential) => {
-      _USER = userCredential.user;
-
-      renderError();
-
-      authForm.classList.add("hidden");
-      interestsForm.classList.remove("hidden");
-    })
-    .catch(renderError);
 });
+// End -- Auth Form Listeners
 
+// Interests Form Listeners -- Begin
 interestsForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
@@ -179,29 +196,31 @@ interestsForm.addEventListener("submit", (event) => {
   choiceForm.classList.remove("hidden");
 });
 
-choiceForm.addEventListener("submit", (event) => {
+choiceForm.addEventListener("submit", async (event) => {
   event.preventDefault();
 
   choiceForm.classList.add("hidden");
 
   switch (event.submitter.name) {
     case "takeToPrompt":
-      getPromptImage();
+      await getPromptImage();
       break;
     case "takeToGuess":
-      getChallenge();
+      await getChallenge();
       break;
   }
 });
+// End -- Interests Form Listeners
 
-promptForm.addEventListener("submit", (event) => {
+// Prompt Form Listeners -- Begin
+promptForm.addEventListener("submit", async (event) => {
   event.preventDefault();
 
   const promptData = new FormData(event.target);
 
   switch (event.submitter.name) {
     case "promptSkipButton":
-      getPromptImage();
+      await getPromptImage();
       break;
     case "promptButton":
       const prompt = promptData.get("promptInput");
@@ -210,20 +229,47 @@ promptForm.addEventListener("submit", (event) => {
         return;
       }
 
-      const createPrompt = httpsCallable(functions, "createPrompt");
-      createPrompt({
-        imageId: _RESPONSE.imageId,
-        interest: _INTEREST,
-        prompt,
-      })
-        .then(getPromptImage)
-        .catch(renderError);
-      break;
+      try {
+        const createPrompt = httpsCallable(functions, "createPrompt");
+        await createPrompt({
+          imageId: _RESPONSE.imageId,
+          interest: _INTEREST,
+          prompt,
+        });
+
+        await getPromptImage();
+      } catch (error) {
+        renderError(error);
+      }
   }
 });
 
 promptInput.addEventListener("input", updateCharCount);
+// End -- Prompt Form Listeners
 
-guessForm.addEventListener("submit", (event) => {
+// Guess Form Listeners -- Begin
+guessForm.addEventListener("submit", async (event) => {
   event.preventDefault();
+
+  let submitterName = event.submitter.name;
+  switch (submitterName) {
+    case "guessSkipButton":
+      await getChallenge();
+      break;
+    case "guessBotButton":
+    case "guessNotButton":
+      try {
+        const submitGuess = httpsCallable(functions, "submitGuess");
+        const response = await submitGuess({
+          promptId: _RESPONSE.promptId,
+          interest: _INTEREST,
+          guess: `is${submitterName.slice(5, 8)}`,
+        });
+
+        await getChallenge(response);
+      } catch (error) {
+        renderError(error);
+      }
+  }
 });
+// End -- Guess Form Listeners
